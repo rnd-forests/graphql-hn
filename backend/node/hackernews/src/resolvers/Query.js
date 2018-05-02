9663e6f0a706b6a0d3ac41ef430b0d01d6221441
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2018-05-02 12:26:16 +0700

    Filter links

--- backend/node/hackernews/src/resolvers/Query.js
+++ backend/node/hackernews/src/resolvers/Query.js
@@ -1,5 +1,16 @@
 function feed(parent, args, context, info) {
-    return context.db.query.links({}, info)
+    let where = {}
+    let filter = args.filter
+    if (filter) {
+        where = {
+            OR: [
+                { url_contains: args.filter },
+                { description_contains: args.filter },
+            ],
+        }
+    }
+
+    return context.db.query.links({ where }, info)
 }
 
 export default {
