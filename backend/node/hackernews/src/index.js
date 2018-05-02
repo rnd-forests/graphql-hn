6dcf890319b38e104718227aed8854c534ac4ca8
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2018-05-02 18:51:21 +0700

    Fix message

--- backend/node/hackernews/src/index.js
+++ backend/node/hackernews/src/index.js
@@ -31,4 +31,4 @@ const server = new GraphQLServer({
         }),
     }),
 })
-server.start(() => console.log('Server is running on http://localhost:4000'))
+server.start(() => console.log('GraphQL server is running...'))
