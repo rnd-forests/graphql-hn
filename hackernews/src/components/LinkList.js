d9f7e50d576563ed8d4107c2bddc45f145e95798
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2018-05-13 03:18:20 +0700

    Merge backend to frontend

---
+++ hackernews/src/components/LinkList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import { graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
+import Link from './Link'
+
+class LinkList extends Component {
+    render() {
+        if (this.props.feedQuery && this.props.feedQuery.loading) {
+            return <div>Loading</div>
+        }
+
+        if (this.props.feedQuery && this.props.feedQuery.error) {
+            return <div>Error</div>
+        }
+
+        let links = this.props.feedQuery.feed.links
+
+        return (
+            <div>{links.map(link => <Link key={link.id} link={link} />)}</div>
+        )
+    }
+}
+
+const FEED_QUERY = gql`
+    query FeedQuery {
+        feed {
+            links {
+                id,
+                url,
+                description,
+                createdAt
+            }
+        }
+    }
+`
+
+export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
