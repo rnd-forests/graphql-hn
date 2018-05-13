0e1473f21ee9a35cdf8754d4a25df2b86fc2a5e0
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2018-05-13 11:49:35 +0700

    Formatting

--- hackernews/postcss.config.js
+++ hackernews/postcss.config.js
@@ -1,8 +1,8 @@
-let tailwindcss = require('tailwindcss');
+let tailwindcss = require('tailwindcss')
 
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.config.js'),
-    require('autoprefixer'),
-  ],
-};
+    require('autoprefixer')
+  ]
+}
